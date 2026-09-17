f6e3c2f6576ff996be41a265cb04c10c849a7d69
elamurg <ela.murgelj@gmail.com>
2026-09-17 09:19:27 +0100

    feat: all algorithms i need to know.

---
+++ graph-ds/minimum-spanning-tree/kruskal.py
@@ -0,0 +1,5 @@
+"""
+Kruskal's Algorithm is a greedy algorithm that finds a minimum spanning tree for a connected 
+weighted graph. It works by sorting all the edges in the graph in ascending order of their weights
+and then adding the smallest edge that doesn't create a cycle in the tree.
+"""
---
+++ sorting-algo/bubble.py
@@ -0,0 +1,9 @@
+"""
+Bubble Sort is a simple sorting algorithm that works by 
+repeatedly swapping the adjacent elements if they are in the wrong order. 
+It gets its name because with each iteration the largest element "bubbles" 
+up to its proper location.It continues this process of swapping until the 
+entire list is sorted in ascending order. 
+The time complexity of Bubble Sort is O(n^2) in the average and worst case scenarios,
+making it inefficient on large lists. However, it is easy to understand and implement.
+"""
